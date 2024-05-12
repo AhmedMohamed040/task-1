c3e98bc25e2f75a9d2eada53bf5a84c2a373d056
AhmedMohamed040 <a.mohamed00019@gmail.com>
2024-05-12 11:04:44 +0300

    feat:cards

--- components/Testimoni.js
+++ components/Testimoni.js
@@ -10,41 +10,33 @@ import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
 const Testimoni = ({
   listTestimoni = [
     {
-      name: "iezh Robert",
+      name: "Andre Donega",
       image: "/assets/people-3.png",
       city: "Warsaw",
       country: "Poland",
-      rating: "4.5",
+      rating: "5",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "So glad I've met Ahmed Salem, an incredible tour guide, with shuch a great knowledge of Egyptian culture and history. Besides, he is very very friendly, kind and with a great sense of humor, which really made my day!  😁Ahmed Salem promised me...",
     },
     {
-      name: "iezh Robert",
+      name: "Winnie",
       image: "/assets/people-3.png",
       city: "Warsaw",
       country: "Poland",
       rating: "4.5",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "Went on a jam-packed 8 hour layover tour with our guide Ahmed Salem, Ahmed came highly recommended and he did not disappoint! We arranged the airport pickup and itinerary ahead of time. Hed made sure we saw the pyramids, learned about their fascinatin ...",
     },
+   
     {
-      name: "iezh Robert",
+      name: "A Adler",
       image: "/assets/people-3.png",
       city: "Warsaw",
       country: "Poland",
       rating: "4.5",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
-    {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
+       "Wonderful tour in Egypt with guides and drivers who made us feel safe and welcome. Arrangements were made from the US with Ahmed via the internet and he was able to customize the trip to our schedule. We were met at the airport and escorted to our ho...",
+       },
   ],
 }) => {
   const settings = {
@@ -88,11 +80,10 @@ const Testimoni = ({
         arrows={false}
         ref={setSliderRef}
         className="flex items-stretch justify-items-stretch"
-        dir="rtl"
       >
         {listTestimoni.map((listTestimonis, index) => (
           <div className="px-3 flex items-stretch" key={index}>
-            <div className="border-2 border-gray-500 hover:border-sky-100 transition-all rounded-lg p-8 flex flex-col">
+            <div className="border-2 h-[350px] border-gray-500 hover:border-sky-500 transition-all rounded-lg p-8 flex flex-col">
               <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
                 <div className="flex order-2 xl:order-1">
                   <Image
@@ -117,7 +108,7 @@ const Testimoni = ({
                   </span>
                 </div>
               </div>
-              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+              <p className="mt-5 break-words text-left overflow-hidden line-clamp-[7]">“{listTestimonis.testimoni}”</p>
             </div>
           </div>
         ))}
@@ -125,13 +116,13 @@ const Testimoni = ({
       <div className="flex w-full items-center justify-end">
         <div className="flex flex-none justify-between w-auto mt-14">
           <div
-            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-sky-100 border hover:bg-sky-100 hover:text-white-500 transition-all text-sky-100 cursor-pointer"
+            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-sky-500 border hover:bg-sky-500 hover:text-white-500 transition-all text-sky-500 cursor-pointer"
             onClick={sliderRef?.slickPrev}
           >
             <ArrowBack className="h-6 w-6 " />
           </div>
           <div
-            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-sky-100 border hover:bg-sky-100 hover:text-white-500 transition-all text-sky-100 cursor-pointer"
+            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-sky-500 border hover:bg-sky-500 hover:text-white-500 transition-all text-sky-500 cursor-pointer"
             onClick={sliderRef?.slickNext}
           >
             <ArrowNext className="h-6 w-6" />
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <SeoHead title='EG-vacation' />
       <Layout>
         <Hero />
-        <Feature />
+       
         <Pricing />
       </Layout>
     </>
